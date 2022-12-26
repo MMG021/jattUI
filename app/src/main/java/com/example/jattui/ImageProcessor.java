fb00fb205367a16e4977893d9ca5129c6d88dfa9
Mohsin Munir <mohsingoraya313@outlook.com>
2022-12-26 21:03:59 +0500

    new

--- app/src/main/java/com/example/jattui/ImageProcessor.java
+++ app/src/main/java/com/example/jattui/ImageProcessor.java
@@ -76,6 +76,9 @@ public class ImageProcessor {
     /**
      * Resize to avoid using too much memory loading big images (e.g.: 2560*1920)
      **/
+
+
+
     private static Bitmap getImageResized(Context context, Uri selectedImage) {
         Bitmap bm;
         int[] sampleSizes = new int[]{5, 3, 2, 1};
